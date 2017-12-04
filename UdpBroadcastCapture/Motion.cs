576e67d027d2621c2507d783fbbd4fea11344625
Aleksandar Varchev <aleksvarchev@gmail.com>
2017-12-04 15:26:25 +0100

    picturedone

---
+++ UdpBroadcastCapture/Motion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpBroadcastCapture
+{
+    public class Motion
+    {
+
+
+
+        public int ID { get; set; }
+
+        public string MotionBool { get; set; }
+
+        public string MotionLevel { get; set; }
+
+        public string Picture { get; set; }
+
+        public string DateTime { get; set; }
+
+        public Motion()
+        {
+        }
+
+        public Motion(string picture)
+        {
+            //this.ID = Service1.NextId++;
+            this.Picture = picture;
+        }
+
+        public Motion(string motionBool, string motionLevel, string picture, string dateTime)
+        {
+            // this.ID = Service1.NextId++;
+            this.MotionBool = motionBool;
+            this.MotionLevel = motionLevel;
+            this.Picture = picture;
+            this.DateTime = dateTime;
+        }
+    }
+}
